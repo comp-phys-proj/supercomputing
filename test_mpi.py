2f8da155d4fec9a8c6a53c62a5ff0c1ce117966b
Rickard Armiento <rickard-gpg@armiento.net>
2021-10-10 23:57:06 +0200

    Update test_mpi.py

--- test_mpi.py
+++ test_mpi.py
@@ -7,14 +7,16 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if rank == 0:
-  print("We have" , size, "processes." )
+  print("We have" , size, "processes.")
   for i in range(1, size):
-    comm.send("Hello " +str(i)+"!" , dest=i, tag=i)
+    comm.send("Hello "+str(i)+"!", dest=i, tag=i)
 
   for i in range(1, size):
     msg = comm.recv(source=i, tag=0)
     print(msg)
-else:  
+else:
   data = comm.recv(source=0, tag=rank)
   msg = "Hello from " +str(rank)+"."
   comm.send(msg, dest=0, tag=0)
+
+comm.Barrier()
